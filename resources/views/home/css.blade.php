cd913efa9d049f03b2cf093e8cf031bead2cb920
ste <nikstel2001@gmail.com>
2024-03-21 18:24:45 -0700

    5

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -6,7 +6,6 @@
 <meta name="keywords" content="" />
 <meta name="author" content="" />
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
-
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/animate.css">
 <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -14,8 +13,6 @@
 <link rel="stylesheet" href="css/bootstrap-datepicker.css">
 <link rel="stylesheet" href="css/jquery.timepicker.css">
 <link rel="stylesheet" href="css/fancybox.min.css">
-
 <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
 <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
-
 <link rel="stylesheet" href="css/style.css">
